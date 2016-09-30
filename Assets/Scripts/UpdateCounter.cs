12e5fdec0632af5372f7cad073c67c4af530f618
Víctor A. Rodríguez (Bit-Man) <victor@bit-man.guru>
2016-09-29 23:53:29 -0300

    Stinky text countdown

---
+++ Assets/Scripts/UpdateCounter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpdateCounter : MonoBehaviour {
+
+	private float counter;
+	private bool running = false;
+
+	public Text label; 
+
+	void Start () {
+		counter = 0;
+	}
+
+	void Update () {
+		if (this.running && counter > 0) {
+			counter -= Time.deltaTime;
+			UpdateCounterLabel ();
+		}
+	}
+
+	private void UpdateCounterLabel() {
+		this.label.text = ((int)this.counter).ToString() + " second(s) left";
+	}
+
+	public void SetCounter(float counter) {
+		this.counter = counter;
+		UpdateCounterLabel ();
+	}
+
+	public void MakeItRun() {
+		this.running = true;
+	}
+
+	public void MakeItStop() {
+		this.running = false;
+	}
+}
